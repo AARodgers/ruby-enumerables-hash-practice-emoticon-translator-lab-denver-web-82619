5c466035001a8dd5d97ea41ba908a937ecf16641
Amanda Rodgers <aarodgerswork@gmail.com>
2019-08-07 22:29:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,8 @@ def load_library(file_path)
 end
 
 
-def get_japanese_emoticon(file_path, emoticons)
-  result = load_library(file_path)['get_emoticon']
+def get_japanese_emoticon(file_path, emoticon)
+  result = load_library(file_path)['get_emoticon'][emoticon]
 end
 
 def get_english_meaning
